b8b3f0c449a141d6bc3408b709ed5bc374392c98
M Atif <chatif476@gmail.com>
2022-02-02 16:24:38 +0500

    Some Changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home flex justify-between flex-wrap h-56 w-full">
+    <div id="bg1" class="bg-green-400 w-32 h-20"></div>
+    <div id="bg2" class="bg-green-400 w-32 h-20"></div>
+    <div id="bg3" class="bg-green-400 w-32 h-20"></div>
+    <div id="bg4" class="bg-green-400 w-32 h-20"></div>
+    <div id="bg5" class="bg-green-400 w-32 h-20"></div>
+    <div id="bg6" class="bg-green-400 w-32 h-20"></div>
+    <div id="bg7" class="bg-green-400 w-32 h-20"></div>
+    <div id="bg8" class="bg-green-400 w-32 h-20"></div>
+    <div id="bg9" class="bg-green-400 w-32 h-20"></div>
+  </div>
+  <div>
+    <button class="px-3 py-2 bg-green-200 text-green-800" @click="api">Generate</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    async api() {
+      console.clear()
+      fetch("https://www.thecolorapi.com/scheme?hex=0047AB&count=10", {
+        method: "GET"
+      }).then(function (response) {
+        return response.json();
+      }).then(function (data) {
+        console.log(data.mode);
+        bg1.style.background = data.colors[0].hex.value
+        bg1.style.background = data.colors[1].hex.value
+        bg2.style.background = data.colors[2].hex.value
+        bg3.style.background = data.colors[3].hex.value
+        bg4.style.background = data.colors[4].hex.value
+        bg5.style.background = data.colors[5].hex.value
+        bg6.style.background = data.colors[6].hex.value
+        bg7.style.background = data.colors[7].hex.value
+        bg8.style.background = data.colors[8].hex.value
+        bg9.style.background = data.colors[9].hex.value
+      })
+      console.log("done")
+    }
+  },
 }
 </script>
